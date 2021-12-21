e3d13384171ac9b33f70c24f876816c15a450634
khushwindersingh1947 <khushwindersinghdhillon79@gmail.com>
2021-12-21 12:36:13 -0400

    Added function to outline borders of objects in image

--- helpers.c
+++ helpers.c
@@ -119,3 +119,69 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     return;
 }
 
+RGBTRIPLE get_edge(int i,int j,int height,int width,RGBTRIPLE image[height][width])
+{
+    int gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    int gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+    int gxi[3]={0}; int gyi[3]={0};
+    int m,n; m=n=0;
+    for(int k=i-1;k<i+2;k++)
+    {
+        for(int l=j-1;l<j+2;l++)
+        {
+            if(valid_pixal(k,l,height,width))
+            {
+                gxi[0]+=gx[m][n]*image[k][l].rgbtRed;
+                gyi[0]+=gy[m][n]*image[k][l].rgbtRed;
+
+                gxi[1]+=gx[m][n]*image[k][l].rgbtBlue;
+                gyi[1]+=gy[m][n]*image[k][l].rgbtBlue;
+
+                gxi[2]+=gx[m][n]*image[k][l].rgbtGreen;
+                gyi[2]+=gy[m][n]*image[k][l].rgbtGreen;
+            }
+            n++;
+        }
+        m++;
+        n=0;
+    }
+    RGBTRIPLE pixal;
+    pixal.rgbtRed=round(sqrt(gxi[0]*gxi[0]+gyi[0]*gyi[0])>255?255:sqrt(gxi[0]*gxi[0]+gyi[0]*gyi[0]));
+    pixal.rgbtBlue=round(sqrt(gxi[1]*gxi[1]+gyi[1]*gyi[1])>255?255:sqrt(gxi[1]*gxi[1]+gyi[1]*gyi[1]));
+    pixal.rgbtGreen=round(sqrt(gxi[2]*gxi[2]+gyi[2]*gyi[2])>255?255:sqrt(gxi[2]*gxi[2]+gyi[2]*gyi[2]));
+    return pixal;
+
+}
+
+
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    //matrix declarations
+
+
+    RGBTRIPLE copy[height][width];
+    //two loops for picking up pixal
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+
+        copy[i][j]=get_edge(i,j,height,width,image);
+
+        }
+    }
+
+    // loop for putting values back to image
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = copy[i][j];
+        }
+    }
+
+
+    return;
+}
