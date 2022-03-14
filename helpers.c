36a619a65d2c21a226d6227573fc379bff2e3ce2
khushwindersingh1947 <khushwindersinghdhillon79@gmail.com>
2022-03-14 02:33:56 -0400

    Added function to make image black and white

--- helpers.c
+++ helpers.c
@@ -2,3 +2,29 @@
 #include <math.h>
 #include <cs50.h>
     // Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    // values for colors
+    float r = 0;
+    float g = 0;
+    float b = 0;
+    //loop for formula
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            r = image[i][j].rgbtRed;
+            b = image[i][j].rgbtBlue;
+            g = image[i][j].rgbtGreen;
+
+            float average = (r + b + g) / 3;
+            int ave = round(average);
+
+            image[i][j].rgbtRed = ave;
+            image[i][j].rgbtBlue = ave;
+            image[i][j].rgbtGreen = ave;
+        }
+    }
+    return;
+}
+
