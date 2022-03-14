6dd282ab3d5d36f94513e262840914bc14b6b15f
khushwindersingh1947 <khushwindersinghdhillon79@gmail.com>
2022-03-14 02:34:46 -0400

    Added function to make image mirror horizontaly

--- helpers.c
+++ helpers.c
@@ -28,3 +28,37 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     return;
 }
 
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    // temp variable for storage
+    RGBTRIPLE temp;
+    //loop for changing pixals
+    for (int i = 0; i < height; i++)
+    {
+        int k = width - 1;
+
+        if (width % 2 == 0)
+        {
+            for (int j = 0; j < width / 2; j++)
+            {
+                temp = image[i][j];
+                image[i][j] = image[i][k];
+                image[i][k] = temp;
+                k--;
+            }
+        }
+        else
+        {
+            for (int j = 0; j < (width - 1) / 2; j++)
+            {
+                temp = image[i][j];
+                image[i][j] = image[i][k];
+                image[i][k] = temp;
+                k--;
+            }
+
+        }
+    }
+    return;
+}
